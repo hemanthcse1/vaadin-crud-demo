f6e30f5d9475ce3c25879746e40e5af4ea17af9b
hemanthcse1 <hemanthcse1@gmail.com>
2024-02-03 11:11:34 +0530

    Initial commit

---
+++ src/main/java/com/hemanth/vaadincruddemo/VaadinCrudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hemanth.vaadincruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinCrudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaadinCrudDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hemanth/vaadincruddemo/VaadinCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemanth.vaadincruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinCrudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
